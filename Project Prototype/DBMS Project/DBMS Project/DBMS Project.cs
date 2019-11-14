a58d20bfe3fa190879baa3439494598efad2968d
kk05088 <49026436+kk05088@users.noreply.github.com>
2019-11-15 00:21:43 +0500

    Create DBMS Project.cs

---
+++ Project Prototype/DBMS Project/DBMS Project/DBMS Project.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBMS_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form f2 = new Form();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GroupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton14_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadioButton11_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
