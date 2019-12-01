06cf29b8ffd1842595a835748e6ee53e971dc3a7
kk05088 <49026436+kk05088@users.noreply.github.com>
2019-12-01 22:38:32 +0500

    Update DBMS Project.cs

--- Project Prototype/DBMS Project/DBMS Project/DBMS Project.cs
+++ Project Prototype/DBMS Project/DBMS Project/DBMS Project.cs
@@ -22,34 +22,9 @@ namespace DBMS_Project
 
         }
 
-        private void RadioButton4_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void RadioButton10_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void Button1_Click(object sender, EventArgs e)
         {
             this.Close();
-
-        }
-
-        private void Button2_Click(object sender, EventArgs e)
-        {
 
         }
 
@@ -57,30 +32,9 @@ namespace DBMS_Project
         {
 
         }
-
-        private void RadioButton14_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void RadioButton11_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void MaskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
-        {
-
-        }
-
         private void Button3_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void CheckedListBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            
         }
 
         private void TextBox2_TextChanged(object sender, EventArgs e)
@@ -108,6 +62,99 @@ namespace DBMS_Project
         private void TextBox5_TextChanged(object sender, EventArgs e)
         {
             textBox5.MaxLength = 7;
+        }
+
+        private void TextBox7_TextChanged(object sender, EventArgs e)
+        {
+            textBox7.MaxLength = 4;
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckBox12_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox6_TextChanged(object sender, EventArgs e)
+        {
+            textBox6.MaxLength = 9;
+        }
+
+        private void TextBox7_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch !=8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char l = e.KeyChar;
+            if (!char.IsLetter(l) && l != 8)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char tr = e.KeyChar;
+            if (!char.IsLetter(tr) && tr != 8)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
