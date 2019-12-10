19363e1cc5c5326f9c86fb59351671e6600391ee
kk05088 <49026436+kk05088@users.noreply.github.com>
2019-12-10 12:04:32 +0500

    Update Logi.Designer.cs

--- Project Prototype/DBMS Project/DBMS Project/Logi.Designer.cs
+++ Project Prototype/DBMS Project/DBMS Project/Logi.Designer.cs
@@ -104,7 +104,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(401, 230);
+            this.ClientSize = new System.Drawing.Size(421, 230);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button1);
