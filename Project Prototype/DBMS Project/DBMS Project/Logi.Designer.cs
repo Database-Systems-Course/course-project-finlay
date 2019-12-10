fe6c15b5ae1acff7572049512867185f14dd7164
kk05088 <49026436+kk05088@users.noreply.github.com>
2019-12-10 17:21:25 +0500

    Update Logi.Designer.cs

--- Project Prototype/DBMS Project/DBMS Project/Logi.Designer.cs
+++ Project Prototype/DBMS Project/DBMS Project/Logi.Designer.cs
@@ -73,7 +73,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(314, 90);
+            this.button1.Location = new System.Drawing.Point(314, 64);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
