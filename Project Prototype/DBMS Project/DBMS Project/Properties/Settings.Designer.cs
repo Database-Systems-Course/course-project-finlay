1add4290303ea7733f890091ad75b93cf31ad7e8
kk05088 <49026436+kk05088@users.noreply.github.com>
2019-12-10 11:50:39 +0500

    Update Settings.Designer.cs

--- Project Prototype/DBMS Project/DBMS Project/Properties/Settings.Designer.cs
+++ Project Prototype/DBMS Project/DBMS Project/Properties/Settings.Designer.cs
@@ -8,22 +8,51 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DBMS_Project.Properties
-{
-
-
+namespace DBMS_Project.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-PBLF4VVJ\\SQLEXPRESS;Initial Catalog=Project-Finlay;Integrated " +
+            "Security=True")]
+        public string Project_FinlayConnectionString {
+            get {
+                return ((string)(this["Project_FinlayConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-PBLF4VVJ\\SQLEXPRESS;Initial Catalog=Project-Finlay(new);Integr" +
+            "ated Security=True")]
+        public string Project_Finlay_new_ConnectionString {
+            get {
+                return ((string)(this["Project_Finlay_new_ConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-PBLF4VVJ\\SQLEXPRESS;Initial Catalog=Project;Integrated Securit" +
+            "y=True")]
+        public string ProjectConnectionString {
+            get {
+                return ((string)(this["ProjectConnectionString"]));
             }
         }
     }
